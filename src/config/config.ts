a56720d875c43a3f085fea80ab4af883a286b1f9
Your Name <your.email@example.com>
2025-05-12 03:08:55 +0300

    connection with remote db has been stablished

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,14 @@
 import { Sequelize } from 'sequelize';
+import * as fs from 'fs';
 require('dotenv').config();
 
-export const sequelize = new Sequelize('Reverence_Number_Service', process.env.DB_USER!, process.env.DB_PASS!, {
+export const sequelize = new Sequelize('ref-num-db', process.env.DB_USER!, process.env.DB_PASS!, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true
+    }
+  }
 });
--- src/services/activeMerchantConsumer.ts
+++ src/services/activeMerchantConsumer.ts
@@ -25,11 +25,36 @@ async function consumeActiveMerchantsQueue() {
                     console.log('Received:', merchantData);
 
                     if (Array.isArray(merchantData)) {
-                        await ActiveMerchants.bulkCreate(merchantData);
-                    } else {
-                        await ActiveMerchants.create(merchantData);
-                    }
-
+                        await ActiveMerchants.bulkCreate(merchantData, {
+                          updateOnDuplicate: [
+                            'legal_name',
+                            'commercial_name',
+                            'address',
+                            'commercial_reg_number',
+                            'license_issue_date',
+                            'license_exp_date',
+                            'tax_id_number',
+                            'telephone_number',
+                            'admin_email',
+                            'business_type',
+                            'bank_name',
+                            'account_holder_name',
+                            'account_type',
+                            'account_number',
+                            'iban',
+                            'swift',
+                            'settlement_time',
+                            'settlement_period',
+                            'commission_setup',
+                            'commission_amount',
+                            'longitude',
+                            'latitude',
+                            'fee_from'
+                          ]
+                        });
+                      } else {
+                        await ActiveMerchants.upsert(merchantData);
+                      }
                     channel.ack(msg);
                 } catch (err) {
                     console.error('Error processing message:', err);
