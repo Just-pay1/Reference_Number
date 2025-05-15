21760ab2f525ad697e31652604df8583d6bdb02c
Youssef Moghazy <youssefmoghazy55@gmail.com>
2025-05-15 22:44:34 +0300

    handle small error

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import './models';
 
 const app = express();
 app.use(express.json());
+app.use('/api/billing', billingRoutes); 
 async function initialize() {
   try {
     await sequelize.authenticate();
